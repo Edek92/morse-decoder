456d8a65f5687bc17a1aa6de159afd610074e140
Edvard Astapovich <edvardastapovich@gmail.com>
2022-07-30 19:23:34 +0400

    feat: ready

--- src/index.js
+++ src/index.js
@@ -38,8 +38,58 @@ const MORSE_TABLE = {
 };
 
 function decode(expr) {
-    // write your solution here
+
+
+	function strToMatrix(string) {
+		let arrMatrix = [];
+		let subarray;
+
+		for (let i = 0; i < string.length; i = i + 10) {
+			subarray = [];
+			for (let j = 0; j < 10; j++) {
+				subarray.push(string[i + j])
+			}
+			arrMatrix.push(subarray.join(''))
+		}
+
+		return arrMatrix;
+	}
+
+
+	function MorseToLetter (element) {
+		let morse = '';
+		let letter;
+
+		if (element === '**********') {
+			return ' ';
+		}
+
+		for (let i = 0; i < element.length; i = i + 2) {
+			if (element[i] + element[i + 1] === '10') {
+				morse += '.';
+			} else if ((`${element[i]}${element[i + 1]}` === '11')) {
+				morse += '-';
+			}
+		}
+
+		for (let i in MORSE_TABLE) {
+			if (i === morse) {
+				letter = MORSE_TABLE[i];
+			}
+		}
+
+		return letter;
+	}
+
+	let result = '';
+
+	strToMatrix(expr).forEach ((elem) => {
+		result += MorseToLetter (elem);
+	})
+
+	return result;
 }
+
 
 module.exports = {
     decode
